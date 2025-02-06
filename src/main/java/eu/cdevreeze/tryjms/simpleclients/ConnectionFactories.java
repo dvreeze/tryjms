4d3fb9567df6c72e0eefd0f1e6b435874f8bb72d
Chris de Vreeze <chris.de.vreeze@caiway.net>
2025-02-06 20:40:00 +0100

    Using reflection in bootstrapping, so IBM JAR can be runtime dependency

--- src/main/java/eu/cdevreeze/tryjms/simpleclients/ConnectionFactories.java
+++ src/main/java/eu/cdevreeze/tryjms/simpleclients/ConnectionFactories.java
@@ -16,12 +16,9 @@
 
 package eu.cdevreeze.tryjms.simpleclients;
 
-import com.ibm.msg.client.jakarta.jms.JmsConnectionFactory;
-import com.ibm.msg.client.jakarta.jms.JmsFactoryFactory;
-import com.ibm.msg.client.jakarta.wmq.WMQConstants;
 import jakarta.jms.ConnectionFactory;
-import jakarta.jms.JMSException;
-import jakarta.jms.JMSRuntimeException;
+
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * ConnectionFactory factory.
@@ -30,21 +27,72 @@ import jakarta.jms.JMSRuntimeException;
  */
 public class ConnectionFactories {
 
+    private static final String WMQ_HOST_NAME = "XMSC_WMQ_HOST_NAME";
+    private static final String WMQ_PORT = "XMSC_WMQ_PORT";
+    private static final String WMQ_CHANNEL = "XMSC_WMQ_CHANNEL";
+    private static final String WMQ_CONNECTION_MODE = "XMSC_WMQ_CONNECTION_MODE";
+    private static final String WMQ_QUEUE_MANAGER = "XMSC_WMQ_QUEUE_MANAGER";
+    private static final String USER_AUTHENTICATION_MQCSP = "XMSC_USER_AUTHENTICATION_MQCSP";
+    private static final String USERID = "XMSC_USERID";
+    private static final String PASSWORD = "XMSC_PASSWORD";
+    private static final int WMQ_CM_CLIENT = 1;
+
     public static ConnectionFactory newConnectionFactory() {
         try {
-            JmsFactoryFactory ff = JmsFactoryFactory.getInstance(WMQConstants.JAKARTA_WMQ_PROVIDER);
-            JmsConnectionFactory cf = ff.createConnectionFactory();
-            cf.setStringProperty(WMQConstants.WMQ_HOST_NAME, System.getProperty("hostName", "localhost"));
-            cf.setIntProperty(WMQConstants.WMQ_PORT, Integer.parseInt(System.getProperty("port", "1414")));
-            cf.setStringProperty(WMQConstants.WMQ_CHANNEL, System.getProperty("channel", "DEV.APP.SVRCONN"));
-            cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
-            cf.setStringProperty(WMQConstants.WMQ_QUEUE_MANAGER, System.getProperty("queueManager", "QM1"));
-            cf.setBooleanProperty(WMQConstants.USER_AUTHENTICATION_MQCSP, true);
-            cf.setStringProperty(WMQConstants.USERID, "app");
-            cf.setStringProperty(WMQConstants.PASSWORD, "passw0rd");
+            Class<?> wmqConstantsClass =
+                    Class.forName("com.ibm.msg.client.jakarta.wmq.WMQConstants");
+            Class<?> jmsFactoryFactoryClass =
+                    Class.forName("com.ibm.msg.client.jakarta.jms.JmsFactoryFactory");
+
+            // Must be "com.ibm.msg.client.jakarta.wmq"
+            String jakartaWmqProviderName = (String) wmqConstantsClass.getField("JAKARTA_WMQ_PROVIDER").get(null);
+            Object jmsFactoryFactory = jmsFactoryFactoryClass
+                    .getMethod("getInstance", String.class)
+                    .invoke(null, jakartaWmqProviderName);
+
+            ConnectionFactory cf = (ConnectionFactory) jmsFactoryFactoryClass
+                    .getMethod("createConnectionFactory")
+                    .invoke(jmsFactoryFactory);
+
+            setStringProperty(cf, WMQ_HOST_NAME, System.getProperty("hostName", "localhost"));
+            setIntProperty(cf, WMQ_PORT, Integer.parseInt(System.getProperty("port", "1414")));
+            setStringProperty(cf, WMQ_CHANNEL, System.getProperty("channel", "DEV.APP.SVRCONN"));
+            setIntProperty(cf, WMQ_CONNECTION_MODE, WMQ_CM_CLIENT);
+            setStringProperty(cf, WMQ_QUEUE_MANAGER, System.getProperty("queueManager", "QM1"));
+            setBooleanProperty(cf, USER_AUTHENTICATION_MQCSP, true);
+            setStringProperty(cf, USERID, "app");
+            setStringProperty(cf, PASSWORD, "passw0rd");
             return cf;
-        } catch (JMSException e) {
-            throw new JMSRuntimeException(e.getMessage(), e.getErrorCode(), e);
+        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException |
+                 NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void setStringProperty(ConnectionFactory cf, String constantName, String constantValue) {
+        try {
+            cf.getClass().getMethod("setStringProperty", String.class, String.class)
+                    .invoke(cf, constantName, constantValue);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void setIntProperty(ConnectionFactory cf, String constantName, int constantValue) {
+        try {
+            cf.getClass().getMethod("setIntProperty", String.class, int.class)
+                    .invoke(cf, constantName, constantValue);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void setBooleanProperty(ConnectionFactory cf, String constantName, boolean constantValue) {
+        try {
+            cf.getClass().getMethod("setBooleanProperty", String.class, boolean.class)
+                    .invoke(cf, constantName, constantValue);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
     }
 }
